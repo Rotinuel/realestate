867f41d9888c23db059c60a71f6ba80812245605
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2023-12-29 00:10:37 +0100

    connecting mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
+// import required packages
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB connected');
+    }).catch((err) => {
+    console.log(err);
+})
+
 
 const app = express();
 
